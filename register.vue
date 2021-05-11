3901a509c9c0a875c5ddf56dd4acc39e5533ac4e
panuwatcomputer2556 <62603087@kmitl.ac.th>
2021-05-11 20:27:58 +0700

    3424

--- register.vue
+++ register.vue
@@ -15,25 +15,24 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script src="Expiry_storedData.js"></script>
 
-
+dfsdfdsfdsfds
 <template>
-    <div>
-      <b-table striped hover :items="items"></b-table>
-    </div>
-  </template>
-  
-  <script>
-    export default {
-      data() {
-        return {
-          items: [
-            { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-            { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-            { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-            { age: 38, first_name: 'Jami', last_name: 'Carney' }
-          ]
-        }
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
       }
     }
-  </script>
-  
+  }
+</script>
